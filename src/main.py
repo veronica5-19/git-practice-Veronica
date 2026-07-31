7c103ebd535329e2b5ae900f29c9afd945e4ae63
veronica <veronicagrande03@gmail.com>
2026-07-31 20:27:03 +0600

    Add error handling for division

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from datetime import date
-from utils import add, subtract, multiply
+from utils import add, subtract, multiply, divide
 
 print("Name: Veronica")
 print("Today's Date:", date.today())
@@ -7,3 +7,5 @@ print("Today's Date:", date.today())
 print("Addition:", add(10, 5))
 print("Subtraction:", subtract(10, 5))
 print("Multiplication:", multiply(10, 5))
+print("Division:", divide(10, 2))
+print("Division Error:", divide(10, 0))
