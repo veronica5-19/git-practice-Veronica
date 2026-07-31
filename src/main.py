4c78f5a4210653fb22265f0f0cf82cafeccb7153
veronica <veronicagrande03@gmail.com>
2026-07-31 20:11:44 +0600

    Add multiply function

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from datetime import date
+from utils import add, subtract, multiply
+
+print("Name: Veronica")
+print("Today's Date:", date.today())
+
+print("Addition:", add(10, 5))
+print("Subtraction:", subtract(10, 5))
+print("Multiplication:", multiply(10, 5))
